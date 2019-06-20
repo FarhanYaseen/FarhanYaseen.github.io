c5dc9191c95fb204dd7f9f109a887debfca73105
Farhan Yaseen <farhan@cloudcade.pk>
2019-06-20 12:00:25 +0500

    Updates

--- src/Language.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Language extends Component {
-	render() {
-		return <li>{this.props.item.name} <span className="lang-desc">({this.props.item.details})</span></li>
-	}
-}
-
-export default Language;
